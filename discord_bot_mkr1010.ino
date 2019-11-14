594298c22cb3fceb80563263d72b9b949baf6464
PuffDaddy5488 <57723183+PuffDaddy5488@users.noreply.github.com>
2019-11-14 12:08:41 -0500

    Update discord_bot_mkr1010.ino

--- discord_bot_mkr1010.ino
+++ discord_bot_mkr1010.ino
@@ -11,18 +11,14 @@
 void setup() {
   Serial.begin(9600);
   connect_to_wifi();
-  discord_send("Yar Har, let's sail the Sea Of Thieves!");
-  discord_send("https://www.youtube.com/watch?v=27mB8verLK8");
+  discord_send("Hello World!");
   delay(3600000);
-  discord_send("Fire The Cannons!");
 }
 
 void loop() {
   Serial.begin(9600);
   connect_to_wifi();
    delay(3600000);
-  discord_send("Yar Har, let's sail the Sea Of Thieves!");
-  discord_send("https://www.youtube.com/watch?v=27mB8verLK8");
+  discord_send("Hello World!");
   delay(3600000);
-  discord_send("Fire The Cannons!");
 }
