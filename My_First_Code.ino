3d80a9039ef6fa0fc99ab92b75d6fef79372d327
PuffDaddy5488 <57723183+PuffDaddy5488@users.noreply.github.com>
2019-11-14 11:30:18 -0500

    Create My_First_Code.ino

---
+++ My_First_Code.ino
@@ -0,0 +1,28 @@
+// Supplies Needed:  Arduino Uno, A breadboard(optional), and two wires.
+int ledPin = (13);
+void setup() {
+pinMode(ledPin, OUTPUT);
+Serial.println(9600, "the led is on");
+String("led is on");
+int(ledPin = 13);
+Serial.println(9600);
+}
+
+void loop() {
+  pinMode(ledPin, OUTPUT);
+  String("led is on");
+  Serial.println(9600, "ledPin");
+delay(1000);
+pinMode(ledPin, OUTPUT);
+digitalWrite(ledPin, 13);
+delay(1000);
+digitalWrite(ledPin, HIGH);
+Serial.println("The led is on");
+delay(1000);
+Serial.println("The led is off");
+digitalWrite(ledPin, LOW);
+Serial.println(9600, "The led is off");
+pinMode(Serial, ledPin);
+pinMode(Serial, OUTPUT);
+Serial.println(9600);
+}
