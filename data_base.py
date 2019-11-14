f32ddd5072d6ee0a01723b879e8c17b708d79754
PuffDaddy5488 <57723183+PuffDaddy5488@users.noreply.github.com>
2019-11-14 11:17:03 -0500

    Update data_base.py

--- data_base.py
+++ data_base.py
@@ -1,14 +1,15 @@
 import time
-
+#import time is for the time.sleep modules
 for x in range(1):
     print("Please Wait While I Fetch The Registration Info...")
+    #The time.sleep(5) is how many seconds it will take for the next command to be implemented.
     time.sleep(5)
-
+#You must type in the information that it asks for so you can interact with the code correctly.
 First_Name = input("Please Enter Your First Name:")
 Last_Name = input("Please Enter Your Last Name:")
-Age = input("Please Enter Your Date of Birth  MM/DD/YYYY:")
 Username = input("Please Enter Your Prefered Username:")
 for i in range(1):
     print("Adding to the Data Base, Please Wait...")
     time.sleep(5)
     print("Hello", Username, "Welocme To The Oasis.")
+#Welcome to The Oasis...
